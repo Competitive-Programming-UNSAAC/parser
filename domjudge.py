99ccdcdd276e9089b926d7e28f5a9a1ce1859b3d
Kleiber Ttito <kleiber100892@gmail.com>
2024-12-06 15:45:17 -0300

    fix

--- domjudge.py
+++ domjudge.py
@@ -18,6 +18,7 @@ problemsFile = config["Metadata"]["problems"]
 submissionsFile = config["Metadata"]["submissions"]
 runsFile = config["Metadata"]["runs"]
 contestantsFile = config["Metadata"]["contestants"]
+judgementsFile = config["Metadata"]["judgements"]
 
 contestDuration = config["Contest"]["duration"]
 contestStart = config["Contest"]["start"]
@@ -172,6 +173,21 @@ def getRuns():
 
     return runsById
 
+def getJudgements():
+    filespathJudgements = os.path.join(metadataDir, judgementsFile)
+    url = f'http://{judgeHost}/api/v4/contests/{contestId}/judgements'
+    judgementsJson = getJsonMetadata(filespathJudgements, url)
+
+    judgements = {}
+
+    for judgement in judgementsJson:
+        submissionId = judgement["submission_id"]
+        if submissionId not in judgements:
+            judgements[submissionId] = []
+        judgements[submissionId].append(judgement)
+
+    return judgements
+
 
 def getSubmissions(teamsById, problemsById):
     filepathSubmissions = os.path.join(metadataDir, submissionsFile)
@@ -206,11 +222,44 @@ def getSubmissions(teamsById, problemsById):
 
     return [s.__dict__ for s in submissions]
 
+def getSubmissionsFromJudgements(teamsById, problemsById):
+    filepathSubmissions = os.path.join(metadataDir, submissionsFile)
+    url = f'http://{judgeHost}/api/v4/contests/{contestId}/submissions'
+    submissionsJson = getJsonMetadata(filepathSubmissions, url)
+
+    judgements = getJudgements()
+    submissions = []
+
+    for submission in submissionsJson:
+        contestTime = submission["contest_time"]
+        problemId = submission["problem_id"]
+        teamId = int(submission["team_id"])
+        submissionId = submission["id"]
+
+        if submissionId not in judgements:
+            continue
+
+        time = datetime.strptime(contestTime, "%H:%M:%S.%f")
+        hours = time.hour
+        minutes = time.minute
+        timeSubmitted = hours*60 + minutes
+
+        contestantName = teamsById[teamId]
+
+        problemIndex = problemsById[problemId]
+
+        veredicts = getVeredicts(judgements[submissionId])
+        verdict = getFinalVeredict(veredicts)
+
+        submissions.append(Submission(timeSubmitted, contestantName, problemIndex, verdict))
+
+    return [s.__dict__ for s in submissions]
+
 def domjudgeScoreboard():
     contestMetadata = getContestMetadata()
     problems, problemsById = getProblems()
     contestants, teamsById = getContestants()
-    submissions = getSubmissions(teamsById, problemsById)
+    submissions = getSubmissionsFromJudgements(teamsById, problemsById)
     scoreboardJson = {
         "contestMetadata" : contestMetadata,
         "problems" : problems,
